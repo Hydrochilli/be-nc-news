5ef5cc00080a415a26488a6811553d11651f92db
Hydrochilli <mainzer10@gmail.com>
2024-07-18 11:30:22 +0100

    Ticket-9

--- models/articles.models.js
+++ models/articles.models.js
@@ -82,6 +82,6 @@ exports.updateArticleVotesByID = async(article_id, inc_votes) => {
 exports.removeCommentByCommentID = async (comment_id) => {
     const {rowCount} = await db.query('DELETE FROM comments WHERE comment_id = $1', [comment_id])
     if(rowCount === 0) {
-        return Promise.reject({status: 404, message: 'No comment wit that ID'})
+        return Promise.reject({status: 404, message: 'No comment with that ID'})
     }
 }
